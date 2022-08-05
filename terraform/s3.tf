51dcbb5252a02ecd55a961fa2c93cea6ef0ad474
Antonio Dell'Elce <antouk@dellelce.com>
2022-08-05 16:10:08 +0100

    update variable name

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,7 +2,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "s3-web-test-${var.common-id}"
+  bucket = "s3-web-test-${var.common_id}"
   acl    = "public-read"
 
   versioning = {
