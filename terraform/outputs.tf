556b40f1c1fd8e77d283260b1ee5ac28a1e436fe
Antonio Dell'Elce <antouk@dellelce.com>
2022-08-05 17:07:43 +0100

    add terraform object name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,6 @@ output "s3_address" {
   value = module.s3_bucket.s3_bucket_bucket_domain_name
 }
 
-
 output "ec2_backend_ip" {
-  value = module.ec2_instance.public_ip
+  value = module.ec2_instance_backend.public_ip
 }
