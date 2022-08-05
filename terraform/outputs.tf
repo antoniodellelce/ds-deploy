ac651e075c3d89bd36add5ec18c31402edad5735
Antonio Dell'Elce <antouk@dellelce.com>
2022-08-05 15:31:09 +0100

    add basic s3 bucke definition

---
+++ terraform/outputs.tf
@@ -0,0 +1,5 @@
+
+output "s3_address" {
+  value = module.s3_bucket.s3_bucket_bucket_domain_name
+}
+
---
+++ terraform/s3.tf
@@ -0,0 +1,12 @@
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "s3-web-test-${var.common-id}"
+  acl    = "public-read"
+
+  versioning = {
+    enabled = false
+  }
+
+}
